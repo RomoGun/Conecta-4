7e286a585c616480d8d87e91885bd9eb828d7d15
Romo_Gun <renathor97@gmail.com>
2025-01-08 23:31:46 -0600

    ParcheHabilitados

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,15 +1,31 @@
-let Habilitados = []
+import { useEffect } from "react";
+
+let Habilitados =  []
+// const habilitadosLocal = window.localStorage.setItem('HabilitadosLocal', JSON.stringify([]));
+
 
 
 export const Square = ({children, isSelected, updateBoard, index}) => {
+  // const [habilitados, setHabilitados] = useState([]);
+  
+
   const className = `square ${isSelected ? 'is-selected' : ''}`
   const handleClick = () => {
-    if(index>=35 || Habilitados.includes(index)){
-        Habilitados.push(index - 7);
-        updateBoard(index);
-      }
-    }
     
+    if(index>=35 || Habilitados.includes(index)){
+      Habilitados.push(index - 7);
+      window.localStorage.setItem('HabilitadosLocal', JSON.stringify(Habilitados));
+      updateBoard(index);
+    }
+  }
+    
+    useEffect(() => {
+      const habilitadosLocal = window.localStorage.getItem('HabilitadosLocal');
+      Habilitados =  JSON.parse(habilitadosLocal) ? JSON.parse(habilitadosLocal) : Habilitados
+      // console.log(Habilitados)
+    }, [Habilitados]);
+    
+
     return(
       <div onClick={handleClick}
       className={className}>
@@ -20,5 +36,10 @@ export const Square = ({children, isSelected, updateBoard, index}) => {
 
 export const ResetHabilitados = () => {
   Habilitados = []
+  window.localStorage.setItem('HabilitadosLocal', JSON.stringify([]));
+  // window.localStorage.removeItem('HabilitadosLocal');
 }
-
+// export const SaveHabilitadosStorage = (newHabilitado) => {
+//   //Guardar Partida
+//   window.localStorage.setItem('HabilitadosLocal', JSON.stringify(newHabilitado));
+// }
